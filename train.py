8db3c7a5a4df10fefdfb498e46360647c0f27d95
Sidhant Subramanian <sidhant.subramanian@gmail.com>
2020-02-01 11:13:16 +0530

    Update train.py

--- train.py
+++ train.py
@@ -35,16 +35,8 @@ model.add(Dense(3, kernel_initializer='normal', activation='tanh'))
 	# Compile model
 model.compile(loss='mean_squared_error', optimizer='Adamax', metrics=[metrics.mae, metrics.categorical_accuracy])
 
-
-# print(Test_X)
-# Train the model
 model.fit(x=Train_X, y=Train_y, batch_size=20, epochs=128,  verbose=2)
 
 scores = model.evaluate(x=Test_X, y=Test_y, batch_size=None, verbose=2, sample_weight=None, steps=None)
 	
 print('Test loss:', scores)	
-
-# print('Test accuracy:', scores[1])
-
-# Train the model, iterating on the data in batches of 32 samples
-# model.fit(data, labels, epochs=10, batch_size=32))
